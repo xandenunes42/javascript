de288c959ce02c462f6dc31ccccbe8cfc8a6143e
Alexandre Nunes <alexandre.oli.nunes@gmail.com>
2025-08-11 20:08:46 -0300

    condicionais

---
+++ Condicionais/script.js
@@ -0,0 +1,29 @@
+const INSURANCE_COST = 2.99;
+
+let shippingCost = 9.99;
+let isOrderValid = true;
+
+let userAge = 20;
+let points = 400;
+let cartValue = 199;
+let hasPromoCode = false;
+let hasParentsApproval = false;
+let addInsurance = true;
+
+/** calculate shipping cost*/
+if ((userAge > 65) || (userAge >= 21 && (hasPromoCode || cartValue > 300 || points > 500))) {
+shippingCost = 0;
+} else if (userAge < 21 && hasParentsApproval) {
+shippingCost = shippingCost - 5;
+} else if (userAge < 21) {
+isOrderValid = false;
+}
+
+/** take account of insurance */
+if (isOrderValid && addInsurance && !hasPromoCode) {
+shippingCost += INSURANCE_COST;
+}
+
+/** show message */
+let message = isOrderValid ? `${shippingCost}` : `Cannot order if under 21`;
+console.log(message);
