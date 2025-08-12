ce30e3602f39c49ff922d2e0c6fac6390d459f18
Alexandre Nunes <alexandre.oli.nunes@gmail.com>
2025-08-11 22:26:00 -0300

    condicionais+switchcase

--- Condicionais/script.js
+++ Condicionais/script.js
@@ -27,3 +27,26 @@ shippingCost += INSURANCE_COST;
 /** show message */
 let message = isOrderValid ? `${shippingCost}` : `Cannot order if under 21`;
 console.log(message);
+
+
+//Switch case:
+
+let gate = prompt("Choose gate: a, b, or c");
+let win = false;
+switch (gate) {
+    case "a":
+     alert("Gate A: empty");
+     break;
+    case "b":
+     alert("Gate B: main prize");
+     win = true;
+     break;
+    case "c":
+     alert("Gate C: empty");
+     break;
+    default:
+     alert("No gate " + String(gate));
+}
+if (win) {
+    alert("Winner!");
+}
