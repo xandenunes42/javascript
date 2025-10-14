3f6de4a444799c0ff0b45e65f22490fd6c3ee360
Alexandre Nunes <alexandre.oli.nunes@gmail.com>
2025-10-13 22:36:02 -0300

    getters&setters

--- Classes/script.js
+++ Classes/script.js
@@ -1,3 +1,4 @@
+/*
 class Vehicle {
     constructor({id, latitude, longitude}){
         this.id = id;
@@ -19,3 +20,29 @@ class Vehicle {
 let vehicle = new Vehicle({longitude: 18.213423, latitude: 59.367628, id: "AL1024"});
 vehicle.setPosition({longitude: 18.193121, latitude: 59.378654});
 console.log(vehicle.getPosition());
+*/
+
+//GETTERS E SETTERS
+
+class Vehicle { 
+    constructor({id, latitude, longitude}){ 
+        this.id = id; 
+        this.position = {latitude, longitude}; 
+        this.status = "unavailable"; 
+    };
+    set position({latitude, longitude}) { 
+        this.time = Date.now(); 
+        this.longitude = longitude; 
+        this.latitude = latitude; 
+    };
+    get position() { 
+        return { 
+            latitude: this.latitude, 
+            longitude: this.longitude 
+        }; 
+    };
+};
+let vehicle = new Vehicle({longitude: 18.213423, latitude: 59.367628, id: "AL1024"});
+vehicle.position = {longitude: 18.193121, latitude: 59.378654};
+console.log(vehicle.position);
+
